e82051df590672765f9fc534d3cef38d6a6827a0
Thomas Ender <73962379+thomasender@users.noreply.github.com>
2021-03-29 11:07:39 +0200

    Add files via upload

--- contracts/Link.sol
+++ contracts/Link.sol
@@ -4,6 +4,6 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Link is ERC20{
   constructor() ERC20("Chainlink", "LINK") public {
-    _mint(msg.sender, 1000);
+    _mint(msg.sender, 100000);
   }
 }
