419e2cc44154537b9fe1237f60bc7a3d7141189d
Thomas Ender <73962379+thomasender@users.noreply.github.com>
2021-03-29 11:06:23 +0200

    Add files via upload

--- migrations/3_link_migration.js
+++ migrations/3_link_migration.js
@@ -3,6 +3,14 @@ const Dex = artifacts.require("Dex");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Link);
-  
+  let link = await Link.deployed();
+  let dex = await Dex.deployed();
 
+  dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]})
+  await link.approve(dex.address, 100000);
+  await dex.deposit(10000, web3.utils.fromUtf8("LINK"));
+  let allowanceDex = await link.allowance(link.address, dex.address);
+  console.log(allowanceDex);
+  let balanceDex = await link.balanceOf(dex.address);
+  console.log(balanceDex);
 };
